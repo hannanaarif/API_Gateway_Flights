9e33aa4d7d8a66d0101aa3c6e23200f5ed193ba9
Aarif Hannan <hannanaarif@gmail.com>
2024-08-23 01:31:05 +0530

    Add new user signup api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,11 @@
 const express=require('express');
 const {InfoController}=require('../../controllers');
+const userRouter=require('./user-routes');
 const router=express.Router();
 
 console.log("Reached V1");
 
 
 router.get('/info',InfoController.info);
-
-
+router.use('/signup',userRouter);
 module.exports=router;
